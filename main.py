3e76f5ca55682ec713a28832430f4fc3f13f4482
Omolix <71171451+Omol1x@users.noreply.github.com>
2022-03-13 17:21:09 +0300

    remove useless module

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
-from fake_useragent import UserAgent
 import os
 from colorama import init
 from colorama import Fore, Back, Style
